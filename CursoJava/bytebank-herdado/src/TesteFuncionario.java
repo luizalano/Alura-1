ece64fe01baa483bbbc6ae9718a90f51556767eb
Luiz Alberto Alano <alano@luigipietro.com.br>
2021-02-08 14:20:07 -0300

    commit 5

---
+++ CursoJava/bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,14 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		Funcionario joao = new Funcionario();
+		
+		joao.setNome("João das Couves");
+		joao.setCpf("14");
+		joao.setSalario(254000);
+		
+
+	}
+
+}
